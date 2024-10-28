ed81d6f7663371f54040f2d151a7cfe8967a44da
Zhylenkova <olena.zhylenkova@gmail.com>
2024-10-28 22:53:09 +0100

    Stwory

---
+++ Klasy/Animals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+    public string Info => $"{Description} <{Size}>";
+}
---
+++ Klasy/Creature.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public Creature()
+    {
+        //blank
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I am {Name} at level {Level}");
+
+    }
+    public string Info => $"{Name} <{Level}>"
+}
