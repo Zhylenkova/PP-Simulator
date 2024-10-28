ccf41ae1c9b46d19f2c47fb92539a1a1573a39c1
Zhylenkova <olena.zhylenkova@gmail.com>
2024-10-28 22:45:23 +0100

    Add project files.

---
+++ Klasy/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Simulator;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Starting Simulator!\n");
+    }
+}
