198c5d55de0afc95a190c1be9d4b1cefa39ca8f8
Zhylenkova <olena.zhylenkova@gmail.com>
2024-12-17 20:52:32 +0100

    Mapy-2

--- Simulator/Maps/SmallMap.cs
+++ Simulator/Maps/SmallMap.cs
@@ -1,7 +1,7 @@
 ﻿namespace Simulator.Maps;
 public abstract class SmallMap : Map
 {
-    List<IMappable>?[,] _fields;
+    //List<IMappable>?[,] _fields;
     protected SmallMap(int sizeX, int sizeY) : base(sizeX, sizeY)
     {
         if (SizeX > 20)
@@ -12,29 +12,8 @@ public abstract class SmallMap : Map
         {
             throw new ArgumentOutOfRangeException(nameof(SizeY), "Too long");
         }
-        _fields = new List<IMappable>?[sizeX, sizeY];
     }
 
-    public override void Add(IMappable mappable, Point position)
-    {
-        if (!Exist(position))
-            throw new ArgumentException("Pozycja poza mapą.");
 
-        _fields[position.X, position.Y] ??= new List<IMappable>();
-        _fields[position.X, position.Y]?.Add(mappable);
-    }
-    public override void Remove(IMappable mappable, Point position)
-    {
-        if (!Exist(position))
-            throw new ArgumentException("Pozycja poza mapą.");
-        _fields[position.X, position.Y]?.Remove(mappable);
-    }
-    public override List<IMappable>? At(Point position)
-    {
-        if (!Exist(position))
-            throw new ArgumentException("Pozycja poza mapą.");
-        return _fields[position.X, position.Y];
-    }
-    public override List<IMappable>? At(int x, int y) => At(new Point(x, y));
 
 }
--- Simulator/Maps/SmallTorusMap.cs
+++ Simulator/Maps/SmallTorusMap.cs
@@ -1,8 +1,14 @@
-﻿namespace Simulator.Maps;
+﻿
+namespace Simulator.Maps;
 
 public class SmallTorusMap : SmallMap
 {
     public SmallTorusMap(int sizeX, int sizeY) : base(sizeX, sizeY) { }
+
+    public override List<IMappable>? At(int x, int y)
+    {
+        throw new NotImplementedException();
+    }
 
     public override Point Next(Point p, Direction d)
     {
